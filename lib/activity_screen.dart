a72f95796ea925d5c75850cefb4442c710de8ba2
arjun <arjunb@live.com>
2020-08-27 14:56:09 +0800

    h

--- lib/activity_screen.dart
+++ lib/activity_screen.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class ActivityScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(''hellp);
+    return Container('');
   }
 }
